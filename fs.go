e62ac392fdf2728c37048939fd90f4c266642724
sqweek <sqweek@gmail.com>
2014-08-04 19:35:57 +0800

    Split 9p line ops into seperate file.
    Ignore errors from Clunk in FidDestroy.
    
    Cleanup wraux in Clunk even if dowrite errors.

--- fs.go
+++ fs.go
@@ -20,164 +20,14 @@ import (
 	"io"
 )
 
-type File struct {
-	srv.File
-	name string
-	mode uint32
-}
-
-type LineFile struct {
-	File
-	rdaux map[*srv.Fid] *ReadAux
-	wraux map[*srv.Fid] *bytes.Buffer
-	rdinit ReadInitFn
-	wrline WriteLineFn
-}
-
-type ReadAux struct {
-	buf *bytes.Buffer
-	lines chan string
-	missed int
-}
-
-func (aux *ReadAux) Send(line string) bool {
-	select {
-	case aux.lines <- line:
-		return true
-	default:
-		aux.missed++
-		return false
-	}
-}
-
-func NewReadAux(data string, useChan bool) *ReadAux {
-	aux := ReadAux{buf: bytes.NewBufferString(data)}
-	if useChan {
-		aux.lines = make(chan string, 10)
-	}
-	return &aux
-}
-
-type WriteLineFn func(string) error
-
-type ReadInitFn func() *ReadAux
-
-func (f *LineFile) Open(fid *srv.FFid, mode uint8) error {
-	if mode & 3 == p.OREAD || mode & 3 == p.ORDWR {
-		_, ok := f.rdaux[fid.Fid]
-		if ok {
-			return srv.Ebaduse
-		}
-		f.rdaux[fid.Fid] = f.rdinit()
-	}
-	return nil
-}
-
-func (f *LineFile) FidDestroy(fid *srv.FFid) {
-	err := f.Clunk(fid)
-	if err != nil {
-		log.Println("Clunk error during FidDestroy: ", err)
-	}
-}
-
-func (f *LineFile) Clunk(fid *srv.FFid) error {
-	rdaux, ok := f.rdaux[fid.Fid]
-	if ok {
-		delete(f.rdaux, fid.Fid)
-		if rdaux.lines != nil {
-			close(rdaux.lines)
-		}
-	}
-	buf, ok := f.wraux[fid.Fid]
-	if ok && buf.Len() > 0 {
-		err := dowrite(buf, f.wrline, true)
-		if err != nil {
-			return err
-		}
-	}
-	delete(f.wraux, fid.Fid)
-	return nil
-}
-
-func (f *LineFile) Read(fid *srv.FFid, data []byte, offset uint64) (int, error) {
-	if f.rdinit == nil {
-		return 0, srv.Eperm
-	}
-	n := 0
-	rdaux, ok := f.rdaux[fid.Fid]
-	if !ok {
-		return 0, srv.Ebaduse
-	}
-	/* fill from existing buffer before blocking */
-	if rdaux.buf.Len() > 0 {
-		n, _ = rdaux.buf.Read(data)
-		if n == len(data) {
-			return n, nil
-		}
-	}
-	if rdaux.lines == nil {
-		return n, nil
-	}
-	line, ok := <-rdaux.lines
-	if !ok {
-		return n, nil
-	}
-	if rdaux.missed != 0 {
-		line = line + fmt.Sprintf("\n<%d lines suppressed>", rdaux.missed)
-		rdaux.missed = 0
-	}
-	if n + len(line) + 1 > len(data) {
-		rdaux.buf.WriteString(line)
-		rdaux.buf.Write([]byte{'\n'})
-		n2, _ := rdaux.buf.Read(data[n:])
-		return n + n2, nil
-	} else {
-		n2 := copy(data[n:], line + "\n")
-		return n + n2, nil
-	}
-}
-
-func (f *LineFile) Write(fid *srv.FFid, data []byte, offset uint64) (int, error) {
-	if f.wrline == nil {
-		return 0, srv.Eperm
-	}
-	buf, ok := f.wraux[fid.Fid]
-	if !ok {
-		buf = bytes.NewBuffer(data)
-		f.wraux[fid.Fid] = buf
-	} else {
-		_, err := buf.Write(data)
-		if err != nil {
-			return 0, err
-		}
-	}
-	err := dowrite(buf, f.wrline, false)
-	if err != nil {
-		return 0, err
-	}
-	return len(data), nil
-}
-
-func dowrite(buf *bytes.Buffer, callback func(string) error, eof bool) error {
-	for {
-		line, err := buf.ReadString('\n')
-		if err == io.EOF {
-			if eof {
-				callback(line)
-			} else {
-				buf.WriteString(line)
-			}
-			return nil
-		}
-		err = callback(strings.TrimRight(line, "\r\n"))
-		if err != nil {
-			return err
-		}
-	}
-}
-
 var uid p.User
 var gid p.Group
+
+var Einval = errors.New("invalid argument")
+var Econnected = errors.New("already connected")
+var Edisconnected = errors.New("not connected")
+
+var root IrcFsRoot
 
 type IrcLogger struct {
 	dir string
@@ -228,8 +78,6 @@ type IrcFsRoot struct {
 	nick *LineFile
 	pong *LineFile
 }
-
-var root IrcFsRoot
 
 type IrcFsChan struct {
 	incoming chan string
@@ -451,10 +299,6 @@ func (root *IrcFsRoot) dial() (net.Conn, error) {
 	}
 	return conn, nil
 }
-
-var Einval = errors.New("invalid argument")
-var Econnected = errors.New("already connected")
-var Edisconnected = errors.New("not connected")
 
 func wrRootCtl(line string) error {
 	if len(line) == 0 || line[0] == '#' {
---
+++ line9p.go
@@ -0,0 +1,165 @@
+/* Implements line-based 9p operations */
+package main
+
+import (
+	"bytes"
+	"code.google.com/p/go9p/p"
+	"code.google.com/p/go9p/p/srv"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type File struct {
+	srv.File
+	name string
+	mode uint32
+}
+
+type LineFile struct {
+	File
+	rdaux map[*srv.Fid] *ReadAux
+	wraux map[*srv.Fid] *bytes.Buffer
+	rdinit ReadInitFn
+	wrline WriteLineFn
+}
+
+type WriteLineFn func(string) error
+
+type ReadInitFn func() *ReadAux
+
+type ReadAux struct {
+	buf *bytes.Buffer
+	lines chan string
+	missed int
+}
+
+func (aux *ReadAux) Send(line string) bool {
+	select {
+	case aux.lines <- line:
+		return true
+	default:
+		aux.missed++
+		return false
+	}
+}
+
+func NewReadAux(data string, useChan bool) *ReadAux {
+	aux := ReadAux{buf: bytes.NewBufferString(data)}
+	if useChan {
+		aux.lines = make(chan string, 10)
+	}
+	return &aux
+}
+
+func (f *LineFile) Open(fid *srv.FFid, mode uint8) error {
+	if mode & 3 == p.OREAD || mode & 3 == p.ORDWR {
+		_, ok := f.rdaux[fid.Fid]
+		if ok {
+			return srv.Ebaduse
+		}
+		f.rdaux[fid.Fid] = f.rdinit()
+	}
+	return nil
+}
+
+func (f *LineFile) FidDestroy(fid *srv.FFid) {
+	f.Clunk(fid)
+}
+
+func (f *LineFile) Clunk(fid *srv.FFid) error {
+	rdaux, ok := f.rdaux[fid.Fid]
+	if ok {
+		delete(f.rdaux, fid.Fid)
+		if rdaux.lines != nil {
+			close(rdaux.lines)
+		}
+	}
+	buf, ok := f.wraux[fid.Fid]
+	if ok && buf.Len() > 0 {
+		delete(f.wraux, fid.Fid)
+		err := dowrite(buf, f.wrline, true)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (f *LineFile) Read(fid *srv.FFid, data []byte, offset uint64) (int, error) {
+	if f.rdinit == nil {
+		return 0, srv.Eperm
+	}
+	n := 0
+	rdaux, ok := f.rdaux[fid.Fid]
+	if !ok {
+		return 0, srv.Ebaduse
+	}
+	/* fill from existing buffer before blocking */
+	if rdaux.buf.Len() > 0 {
+		n, _ = rdaux.buf.Read(data)
+		if n == len(data) {
+			return n, nil
+		}
+	}
+	if rdaux.lines == nil {
+		return n, nil
+	}
+	line, ok := <-rdaux.lines
+	if !ok {
+		return n, nil
+	}
+	if rdaux.missed != 0 {
+		line = line + fmt.Sprintf("\n<%d lines suppressed>", rdaux.missed)
+		rdaux.missed = 0
+	}
+	if n + len(line) + 1 > len(data) {
+		rdaux.buf.WriteString(line)
+		rdaux.buf.Write([]byte{'\n'})
+		n2, _ := rdaux.buf.Read(data[n:])
+		return n + n2, nil
+	} else {
+		n2 := copy(data[n:], line + "\n")
+		return n + n2, nil
+	}
+}
+
+func (f *LineFile) Write(fid *srv.FFid, data []byte, offset uint64) (int, error) {
+	if f.wrline == nil {
+		return 0, srv.Eperm
+	}
+	buf, ok := f.wraux[fid.Fid]
+	if !ok {
+		buf = bytes.NewBuffer(data)
+		f.wraux[fid.Fid] = buf
+	} else {
+		_, err := buf.Write(data)
+		if err != nil {
+			return 0, err
+		}
+	}
+	err := dowrite(buf, f.wrline, false)
+	if err != nil {
+		return 0, err
+	}
+	return len(data), nil
+}
+
+func dowrite(buf *bytes.Buffer, callback func(string) error, eof bool) error {
+	for {
+		line, err := buf.ReadString('\n')
+		if err == io.EOF {
+			if eof {
+				callback(line)
+			} else {
+				buf.WriteString(line)
+			}
+			return nil
+		}
+		err = callback(strings.TrimRight(line, "\r\n"))
+		if err != nil {
+			return err
+		}
+	}
+}
+
